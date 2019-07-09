f398759d975693106f3b10f00e8cb34910285cda
k0201 <49719920+k0201@users.noreply.github.com>
2019-07-09 13:43:31 +0200

    podłączenie nowych plików pod projekt

--- SDUP_huffman_encoder_viv18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/xsim_webtalk.tcl
+++ SDUP_huffman_encoder_viv18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir C:/SDUP_huffman_encoder_v18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/
+webtalk_init -webtalk_dir C:/Users/WSN01/Documents/Michal Grzegorzek/trunk/SDUP_huffman_encoder_viv18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Jun 10 10:46:49 2019" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Jul  9 13:41:24 2019" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -12,21 +12,21 @@ webtalk_add_data -client project -key target_family -value "not_applicable" -con
 webtalk_add_data -client project -key target_device -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_package -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
-webtalk_add_data -client project -key random_id -value "aa225e8b-d849-4681-8146-82b09691b7b2" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "3f077cf7-5457-4bfd-9a73-f6d55aaa2eed" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "f723d5dbb2dc4d85838b64458a3719d0" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "12" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "9" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
-webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-5200U CPU @ 2.20GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "2195 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-7500U CPU @ 2.70GHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "2904 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "11 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "3 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "5652_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2453432800 -regid "" -xml C:/SDUP_huffman_encoder_v18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/SDUP_huffman_encoder_v18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/SDUP_huffman_encoder_v18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5756_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2428185028 -regid "" -xml C:/Users/WSN01/Documents/Michal Grzegorzek/trunk/SDUP_huffman_encoder_viv18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/WSN01/Documents/Michal Grzegorzek/trunk/SDUP_huffman_encoder_viv18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/WSN01/Documents/Michal Grzegorzek/trunk/SDUP_huffman_encoder_viv18.2/SDUP_huffman_encoder_v18.2.sim/sim_1/behav/xsim/xsim.dir/huffman_encoder_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
