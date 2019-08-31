23a19b5549538cfa235c63de756e862abeb4979a
Marek Krupa <marrek.krupa@gmail.com>
2019-08-31 19:01:32 +0200

    Czyszczę dysk

--- Matlab/Randomize_vector.m
+++
@@ -1,9 +0,0 @@
-function [randomized_vector] = Randomize_vector(vector)
-
-    local_vector = ones(1, length(vector));
-
-    radnom_index = randperm(length(vector));
-    local_vector(1, radnom_index) = vector(1,:);
-
-    randomized_vector = local_vector;
-end
